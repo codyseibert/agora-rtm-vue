b3f4eedc3afa630f0dcb4eda7740b101cddff044
Cody Seibert <cseibert113@gmail.com>
2022-06-15 06:59:17 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,148 @@
+<script setup>
+import AgoraRTM from 'agora-rtm-sdk';
+import { v4 as uuidv4 } from 'uuid';
+import { ref, onMounted, nextTick } from 'vue';
+
+const APP_ID = '452f99a0814b44d29d9a446ec20356fc';
+const CHANNEL = 'wdj';
+let client = AgoraRTM.createInstance(APP_ID);
+let uid = uuidv4();
+let text = ref('');
+let messagesRef = ref(null);
+let messages = ref([]);
+let channel;
+
+const appendMessage = async (message) => {
+  messages.value.push(message);
+  await nextTick();
+  messagesRef.value.scrollTop =
+    messagesRef.value.scrollHeight;
+};
+
+onMounted(async () => {
+  await client.login({ uid, token: null });
+  channel = await client.createChannel(CHANNEL);
+  await channel.join();
+  channel.on('ChannelMessage', (message, peerId) => {
+    appendMessage({
+      text: message.text,
+      uid: peerId,
+    });
+  });
+});
+
+function sendMessage() {
+  if (text.value === '') return;
+  channel.sendMessage({ text: text.value, type: 'text' });
+  appendMessage({
+    text: text.value,
+    uid,
+  });
+  text.value = '';
+}
+</script>
+
+<template>
+  <div class="panel">
+    <div class="messages" ref="messagesRef">
+      <div class="inner">
+        <div
+          :key="index"
+          v-for="(message, index) in messages"
+          class="message"
+        >
+          <div v-if="message.uid === uid" class="user-self">
+            You:&nbsp;
+          </div>
+          <div v-else class="user-them">Them:&nbsp;</div>
+          <div class="text">{{ message.text }}</div>
+        </div>
+      </div>
+    </div>
+
+    <form @submit.prevent="sendMessage">
+      <input v-model="text" />
+      <button>+</button>
+    </form>
+  </div>
+</template>
+
+<style>
+body {
+  margin: 0;
+  height: 400px;
+}
+
+#app {
+  padding: 2em;
+  height: 100%;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  background: linear-gradient(
+    90deg,
+    rgba(188, 255, 147, 1) 0%,
+    rgba(88, 245, 158, 1) 53%,
+    rgba(0, 237, 69, 1) 100%
+  );
+}
+
+.panel {
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+  margin: 0 auto;
+  max-width: 300px;
+  height: 300px;
+  background: rgba(255, 255, 255, 0.7);
+  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+  backdrop-filter: blur(4px);
+  border-radius: 10px;
+  border: 1px solid rgba(255, 255, 255, 0.18);
+}
+.messages {
+  height: 100%;
+  width: 100%;
+  overflow-y: scroll;
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
+  background-color: white;
+}
+.inner {
+  padding: 10px;
+}
+.message {
+  text-align: left;
+  display: flex;
+  margin-bottom: 6px;
+}
+.user-self {
+  color: green;
+}
+.user-them {
+  color: red;
+}
+form {
+  position: relative;
+  display: flex;
+}
+input {
+  width: 100%;
+  border: none;
+  height: 20px;
+  padding: 8px;
+  border-top: 1px solid #999;
+  border-radius: 0px;
+  outline: none;
+}
+button {
+  border: none;
+  outline: none;
+  background: none;
+  position: absolute;
+  right: 3px;
+  top: 4px;
+  font-size: 24px;
+}
+</style>
